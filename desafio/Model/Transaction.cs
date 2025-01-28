8d8d00a38a4ed2609409bc09699a4690834e8285
caiobraga98 <kaio.braga98@gmail.com>
2025-01-28 15:35:05 -0300

    adcionando pacotes e criando models

---
+++ desafio/Model/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace desafio.Model
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string FromUserId { get; set; }
+        public string ToUserId { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ desafio/Model/Wallet.cs
@@ -0,0 +1,9 @@
+﻿namespace desafio.Model
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
